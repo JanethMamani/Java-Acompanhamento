e8babef41fb450f7d9a101587db4551067a4ff56
Janeth Mamani Sacari <janeth@ibm.com>
2021-08-18 12:42:08 -0300

    Seçao 14 P130 terminada e Projeto P131 criado

--- Seção 14/P130Polimorfismo/src/aplicacion/Programa.java
+++ Seção 14/P130Polimorfismo/src/aplicacion/Programa.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import entidades.Funcionario;
+import entidades.Terceirizado;
 
 public class Programa {
 
@@ -16,8 +17,36 @@ public class Programa {
 		
 		List<Funcionario> funcionarios = new ArrayList<>();
 		
-		System.out.println("Entre com o número de funcionarios: ");
+		System.out.print("Entre com o número de funcionarios: ");
 		int number = sc.nextInt();
+		
+		for (int i=1; i<=number; i++) {
+			System.out.println("Dados do funcionário #" + i + ":");
+			System.out.print("Terceirizado (s/n)? ");
+			char letra = sc.next().charAt(0);
+			System.out.print("Nome: ");
+			sc.nextLine();
+			String nome = sc.nextLine();
+			System.out.print("Horas: ");
+			int horas = sc.nextInt();
+			System.out.print("Valor por hora: ");
+			double valorHora = sc.nextDouble();
+			
+			if (letra == 's') {
+				System.out.print("Despesa adicional: ");
+				double gastoAdicional = sc.nextDouble();
+				Funcionario desconheco = new Terceirizado(nome, horas, valorHora, gastoAdicional);
+				funcionarios.add(desconheco);
+			} else {
+				funcionarios.add(new Funcionario(nome, horas, valorHora));
+			}
+			//Se colocar aqui a lista imprime toda hora no fim de cada funcionario
+			}
+		System.out.println();
+		System.out.println("Pagamentos:");
+		for (Funcionario persona: funcionarios) {
+			System.out.println(persona.getNome() + " - $ " + String.format("%.2f", persona.pagamento()));
+		}
 		
 		sc.close();
 
