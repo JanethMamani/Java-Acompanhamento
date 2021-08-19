84575399dcbdde8516dc4ba75c69921273162abe
Janeth Mamani Sacari <janeth@ibm.com>
2021-08-19 16:21:24 -0300

    P142 Exceptions criado para versionamento - V2

--- Seção 15/P142Exceptions/src/aplicacion/Executor.java
+++ Seção 15/P142Exceptions/src/aplicacion/Executor.java
@@ -1,9 +1,33 @@
 package aplicacion;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+import entidades.Reserva;
+
 public class Executor {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat formato = new SimpleDateFormat("dd/mm/yyyy");
+		
+		System.out.print("Numero do quarto: ");
+		int numero = sc.nextInt();
+		System.out.print("Data de entrada (dd/MM/yyyy): ");
+		Date entrada = formato.parse(sc.next());
+		System.out.print("Data de saida (dd/MM/yyyy): ");
+		Date saida = formato.parse(sc.next());
+		
+		if (!saida.after(entrada)) {
+			System.out.println("Erro em fazer a reserva: A data de saida de ser depois da de entrada");
+		}
+		else {
+			Reserva reserva = new Reserva(numero, entrada, saida);
+			System.out.println("Reseva: " + reserva);
+		}
 
 	}
 
---
+++ Seção 15/P142Exceptions/src/entidades/Reserva.java
@@ -0,0 +1,54 @@
+package entidades;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+public class Reserva {
+	
+	private Integer numeroQuarto;
+	private Date entrada;
+	private Date saida;
+	
+	private static SimpleDateFormat formato = new SimpleDateFormat("dd/mm/yyyy");
+	
+	public Reserva(Integer numeroQuarto, Date entrada, Date saida) {
+		this.numeroQuarto = numeroQuarto;
+		this.entrada = entrada;
+		this.saida = saida;
+	}
+	
+	public Integer getNumeroQuarto() {
+		return numeroQuarto;
+	}
+	public void setNumeroQuarto(Integer numeroQuarto) {
+		this.numeroQuarto = numeroQuarto;
+	}
+	public Date getEntrada() {
+		return entrada;
+	}
+	public Date getSaida() {
+		return saida;
+	}
+	
+	public long duracao() {
+		long dif = saida.getTime() - entrada.getTime();
+		return TimeUnit.DAYS.convert(dif, TimeUnit.MILLISECONDS);
+	}
+	public void atualizarDatas(Date checkin, Date checkout) {
+		entrada = checkin;
+		saida = checkout;
+	}
+	@Override
+	public String toString() {
+		return "Quarto "
+				+ numeroQuarto
+				+ ", Entrada: "
+				+ formato.format(entrada)
+				+ ", Saída: "
+				+ formato.format(saida)
+				+ ", "
+				+ duracao()
+				+ " noites.";
+	}
+}
