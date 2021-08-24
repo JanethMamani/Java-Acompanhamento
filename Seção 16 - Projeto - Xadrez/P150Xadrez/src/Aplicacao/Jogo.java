e7c41254736c0165357c64190e1c7eae7c8feccd
Janeth Mamani Sacari <janeth@ibm.com>
2021-08-24 11:23:04 -0300

    P150 Classes tabuleiro e peca modificadas

--- Seção 16 - Projeto - Xadrez/P150Xadrez/src/Aplicacao/Jogo.java
+++ Seção 16 - Projeto - Xadrez/P150Xadrez/src/Aplicacao/Jogo.java
@@ -1,12 +1,15 @@
 package Aplicacao;
 
 import Tabuleiro.Posicao;
+import Tabuleiro.Tabuleiro;
 
 public class Jogo {
 
 	public static void main(String[] args) {
 		Posicao pos = new Posicao(3, 5);
 		System.out.println(pos);
+		
+		Tabuleiro tabua = new Tabuleiro(8, 8);
 
 	}
 
