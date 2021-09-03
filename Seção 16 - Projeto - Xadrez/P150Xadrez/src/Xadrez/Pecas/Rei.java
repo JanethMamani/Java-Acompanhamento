3c9f1c7e0ece45a62f62ea3ae69ece8aba292e24
Janeth Mamani Sacari <janeth@ibm.com>
2021-09-03 11:48:51 -0300

    P150 Movimentos possiveis do rei

--- Seção 16 - Projeto - Xadrez/P150Xadrez/src/Xadrez/Pecas/Rei.java
+++ Seção 16 - Projeto - Xadrez/P150Xadrez/src/Xadrez/Pecas/Rei.java
@@ -1,23 +1,72 @@
 package Xadrez.Pecas;
 
+import Tabuleiro.Posicao;
 import Tabuleiro.Tabuleiro;
 import Xadrez.Cor;
 import Xadrez.PecaXadrez;
 
-public class Rei extends PecaXadrez{
+public class Rei extends PecaXadrez {
 
 	public Rei(Tabuleiro tabuleiro, Cor cor) {
 		super(tabuleiro, cor);
 	}
-	
+
 	@Override
 	public String toString() {
 		return "R";
 	}
 
+	private boolean podeMover(Posicao posicao) {
+		PecaXadrez pecinha = (PecaXadrez) getTabuleiro().peca(posicao);
+		return pecinha == null || pecinha.getCor() != getCor();
+	}
+
 	@Override
 	public boolean[][] movimentosPossiveis() {
 		boolean[][] malha = new boolean[getTabuleiro().getLinhas()][getTabuleiro().getColunas()];
+
+		Posicao posi = new Posicao(0, 0);
+
+		// Acima
+		posi.setValor(posicao.getLinha() - 1, posicao.getColuna());
+		if (getTabuleiro().posicaoExiste(posi) && podeMover(posi)) {
+			malha[posi.getLinha()][posi.getColuna()] = true;
+		}
+		// Abaixo
+		posi.setValor(posicao.getLinha() + 1, posicao.getColuna());
+		if (getTabuleiro().posicaoExiste(posi) && podeMover(posi)) {
+			malha[posi.getLinha()][posi.getColuna()] = true;
+		}
+		// Esquerda
+		posi.setValor(posicao.getLinha(), posicao.getColuna() - 1);
+		if (getTabuleiro().posicaoExiste(posi) && podeMover(posi)) {
+			malha[posi.getLinha()][posi.getColuna()] = true;
+		}
+		// Direita
+		posi.setValor(posicao.getLinha(), posicao.getColuna() + 1);
+		if (getTabuleiro().posicaoExiste(posi) && podeMover(posi)) {
+			malha[posi.getLinha()][posi.getColuna()] = true;
+		}
+		// NO
+		posi.setValor(posicao.getLinha() - 1, posicao.getColuna() - 1);
+		if (getTabuleiro().posicaoExiste(posi) && podeMover(posi)) {
+			malha[posi.getLinha()][posi.getColuna()] = true;
+		}
+		// NE
+		posi.setValor(posicao.getLinha() - 1, posicao.getColuna() + 1);
+		if (getTabuleiro().posicaoExiste(posi) && podeMover(posi)) {
+			malha[posi.getLinha()][posi.getColuna()] = true;
+		}
+		// SO
+		posi.setValor(posicao.getLinha() + 1, posicao.getColuna() - 1);
+		if (getTabuleiro().posicaoExiste(posi) && podeMover(posi)) {
+			malha[posi.getLinha()][posi.getColuna()] = true;
+		}
+		// SE
+		posi.setValor(posicao.getLinha() + 1, posicao.getColuna() + 1);
+		if (getTabuleiro().posicaoExiste(posi) && podeMover(posi)) {
+			malha[posi.getLinha()][posi.getColuna()] = true;
+		}
 		return malha;
 	}
 
