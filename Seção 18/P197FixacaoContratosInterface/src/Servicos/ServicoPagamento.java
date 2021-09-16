4cd6a2922ba34ec075be5fcd279b90364664a913
Janeth Mamani Sacari <janeth@ibm.com>
2021-09-16 11:53:49 -0300

    P197 Solucao com datas implementadas

--- Seção 18/P197FixacaoContratosInterface/src/Servicos/ServicoPagamento.java
+++ Seção 18/P197FixacaoContratosInterface/src/Servicos/ServicoPagamento.java
@@ -1,6 +1,7 @@
 package Servicos;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import Entidades.Contrato;
@@ -9,7 +10,7 @@ public class ServicoPagamento {
 	
 	private int quantParcelas;
 	
-	SimpleDateFormat dias = new SimpleDateFormat("dd/mm/yyyy");
+	SimpleDateFormat dias = new SimpleDateFormat("dd/MM/yyyy");
 	
 	private Contrato contrato;
 	
@@ -25,10 +26,16 @@ public class ServicoPagamento {
 	public void pagamentoTotal() {
 		Double parcelas = contrato.getValor() / quantParcelas;
 		
+		Calendar calendario = Calendar.getInstance();
+		calendario.setTime(contrato.getData());
+		int mes = 1 + calendario.get(Calendar.MONTH);
+		int dia = calendario.get(Calendar.DAY_OF_MONTH);
+		int ano = calendario.get(Calendar.YEAR);
+		
 		System.out.println("Pagamentos: ");
 		for (int i=1; i<=quantParcelas; i++) {
-			
-			System.out.println(contrato.getData() + "-" + String.format("%.2f", taxas.aplicarTaxa(parcelas, i)));
+			mes = mes + 1;
+			System.out.println(dia + "/" + mes + "/" + ano + " - " + String.format("%.2f", taxas.aplicarTaxa(parcelas, i)));
 		}
 	}
 
