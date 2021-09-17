43c64d17547b3e707e4e6f5bb4d741adfafd9db2
Janeth Mamani Sacari <janeth@ibm.com>
2021-09-17 12:28:25 -0300

    P201 Criacao de Interface Comparable

---
+++ Seção 18/P201InterfaceComparable/src/Aplicacao/Programa.java
@@ -0,0 +1,35 @@
+package Aplicacao;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import Entidade.Funcionario;
+
+public class Programa {
+	
+	public static void main(String[] args) {
+		
+		List<Funcionario> nomes = new ArrayList<>();
+		String caminho = "TestFile.txt";
+		
+		try(BufferedReader br = new BufferedReader(new FileReader(caminho))){
+			String empregado = br.readLine();
+			while (empregado != null) {
+				String[] emp = empregado.split(", ");
+				nomes.add(new Funcionario(emp[0], Double.parseDouble(emp[1])));
+				empregado = br.readLine();
+			}
+			Collections.sort(nomes); //Ordenar nomes em ordem alfabetica
+			for (String item : nomes) {
+				System.out.println(item);
+			}
+		}catch (IOException excep) {
+			System.out.println("Erro: " + excep.getMessage());
+		}
+	}
+
+}
