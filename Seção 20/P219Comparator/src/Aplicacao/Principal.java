9f1250ae5e3bc90e96b1f32a1c343cddf9174658
Janeth Mamani Sacari <janeth@ibm.com>
2021-09-24 13:43:21 -0300

    P219 Experiência Comparator - expressão Lambda

--- Seção 20/P219Comparator/src/Aplicacao/Principal.java
+++ Seção 20/P219Comparator/src/Aplicacao/Principal.java
@@ -16,11 +16,9 @@ public class Principal {
 		produtos.add(new Produto("Notebook", 1200.00));
 		produtos.add(new Produto("Tablet", 450.00));
 		
-		Comparator<Produto> paraComparar = (produto1, produto2) -> {
-			return produto1.getNome().toUpperCase().compareTo(produto2.getNome().toUpperCase());
-		};
+		//Comparator<Produto> paraComparar =
 		
-		produtos.sort(paraComparar);
+		produtos.sort((produto1, produto2) -> produto1.getNome().toUpperCase().compareTo(produto2.getNome().toUpperCase()));
 		
 		for(Produto product : produtos) {
 			System.out.println(product);
