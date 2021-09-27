20b66251f75705b79c431f68da4f0efe6c9f9e3e
Janeth Mamani Sacari <janeth@ibm.com>
2021-09-27 10:36:52 -0300

    P220 Programação funcional e calculo lambda diferente de expressao lambda

--- Seção 20/P220ProgramaFuncionalLambda/src/Aplicacao/Programa.java
+++ Seção 20/P220ProgramaFuncionalLambda/src/Aplicacao/Programa.java
@@ -3,24 +3,22 @@ package Aplicacao;
 import java.util.ArrayList;
 import java.util.List;
 
-import Entidades.Produto;
+import Entidade.Produto;
 
-public class Principal {
+public class Programa {
+	
+	public static int compararProdutos(Produto produto1, Produto produto2) {
+		return produto1.getPreco().compareTo(produto2.getPreco());
+	}
 	
 	public static void main(String[] args) {
-		
 		List<Produto> produtos = new ArrayList<>();
-		
 		produtos.add(new Produto("TV", 900.00));
 		produtos.add(new Produto("Notebook", 1200.00));
 		produtos.add(new Produto("Tablet", 450.00));
 		
-		//Comparator<Produto> paraComparar =
+		produtos.sort(Programa :: compararProdutos);
 		
-		produtos.sort((produto1, produto2) -> produto1.getNome().toUpperCase().compareTo(produto2.getNome().toUpperCase()));
-		
-		for(Produto product : produtos) {
-			System.out.println(product);
-		}
+		produtos.forEach(System.out::println);
 	}
 }
