092802df30eaf6facb14085bd70f6e1568d763d3
Janeth Mamani Sacari <janeth@ibm.com>
2021-09-27 14:10:48 -0300

    P222 - Predicate - Implementação resumida

---
+++ Seção 20/P222Predicate/src/Aplicacao/ProgramaExercicio.java
@@ -0,0 +1,25 @@
+package Aplicacao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Integrantes.Hunter;
+
+public class ProgramaExercicio {
+	
+	public static void main(String[] args) {
+		
+		List<Hunter> hunters = new ArrayList<>();
+		
+		hunters.add(new Hunter("X", "B", 30.0));
+		hunters.add(new Hunter("Zero", "A", 32.0));
+		hunters.add(new Hunter("Iris", "S", 48.0));
+		hunters.add(new Hunter("Axl", "B", 40.0));
+		
+		hunters.removeIf(hunter -> hunter.getHP() >= 40);
+		
+		for(Hunter integrante : hunters) {
+			System.out.println(integrante);
+		}
+	}
+}
---
+++ Seção 20/P222Predicate/src/Integrantes/Hunter.java
@@ -0,0 +1,46 @@
+package Integrantes;
+
+public class Hunter {
+	
+	private String nome;
+	private String rank;
+	private Double HP;
+	
+	public Hunter(String nome, String rank, Double hP) {
+		super();
+		this.nome = nome;
+		this.rank = rank;
+		HP = hP;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getRank() {
+		return rank;
+	}
+
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+
+	public Double getHP() {
+		return HP;
+	}
+
+	public void setHP(Double hP) {
+		HP = hP;
+	}
+
+	@Override
+	public String toString() {
+		return "Hunter: " + nome + "\nRank: " + rank + "; \nHP = " + HP;
+	}
+	
+	
+}
