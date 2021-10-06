45a4ad550d0dd47e1d45319353d89cbe5c240268
Janeth Mamani Sacari <janeth@ibm.com>
2021-10-06 13:35:06 -0300

    P244 Criação da classe departamento

---
+++ Seção 21/P243_ConceitosDAO/src/Aplicacao/ProgramaPrincipal.java
@@ -0,0 +1,13 @@
+package Aplicacao;
+
+import Entidades.Departamento;
+
+public class ProgramaPrincipal {
+	
+	public static void main(String[] args) {
+		
+		Departamento livros = new Departamento(1, "Livros");
+		System.out.println(livros);
+	}
+
+}
