85ee14fd40c32704c9bfd20bf0da6840b4392580
Janeth Mamani Sacari <janeth@ibm.com>
2021-10-20 17:10:50 -0300

    P272 - Janela principal configurada

--- Seção 23/P271-JavaFXcomJDBC/src/application/Main.java
+++ Seção 23/P271-JavaFXcomJDBC/src/application/Main.java
@@ -1,22 +1,26 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			FXMLLoader carregador = new FXMLLoader(getClass().getResource("/InterfaceUsuario/EntradaPrincipal.fxml"));
+			Parent parent = carregador.load();
+			Scene scenePrincipal = new Scene(parent);
+			primaryStage.setScene(scenePrincipal);
+			primaryStage.setTitle("Exemplo aplicação Java FX");
 			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+		} catch(IOException excep) {
+			excep.printStackTrace();
 		}
 	}
 	
