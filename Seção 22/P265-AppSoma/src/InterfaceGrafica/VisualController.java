d3eef46dea6808294f09cde5ae510a45c04ecb75
= <=>
2022-03-02 14:15:45 -0300

    Tratamento de exceção envolvendo NumberFormatException

--- Seção 22/P265-AppSoma/src/InterfaceGrafica/VisualController.java
+++ Seção 22/P265-AppSoma/src/InterfaceGrafica/VisualController.java
@@ -1,29 +1,35 @@
 package InterfaceGrafica;
 
+import Utilidades.Alertas;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class VisualController {
-	
+
 	@FXML
 	private TextField txtNumero1;
-	
+
 	@FXML
 	private TextField txtNumero2;
-	
+
 	@FXML
 	private Label labelResultado;
-	
+
 	@FXML
 	private Button btSoma;
 
 	@FXML
 	public void onBotaoSumAction() {
-		double numero1 = Double.parseDouble(txtNumero1.getText());
-		double numero2 = Double.parseDouble(txtNumero2.getText());
-		double soma = numero1 + numero2;
-		labelResultado.setText(String.format("%.2f", soma));
+		try {
+			double numero1 = Double.parseDouble(txtNumero1.getText());
+			double numero2 = Double.parseDouble(txtNumero2.getText());
+			double soma = numero1 + numero2;
+			labelResultado.setText(String.format("%.2f", soma));
+		} catch (NumberFormatException exp) {
+			Alertas.mostrarAlerta("Erro ao calcular", null, "Insira um número válido!\n" + exp.getMessage(), AlertType.ERROR);
+		}
 	}
 }
